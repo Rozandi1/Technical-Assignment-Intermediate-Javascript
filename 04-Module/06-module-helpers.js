45e45fcceb37e82c562ba6b6c5d93f186231ba91
Rozandi1 <98866052+Rozandi1@users.noreply.github.com>
2022-02-26 20:29:48 +0800

    technical assignment intermediate javascript

--- 04-Module/06-module-helpers.js
+++ 04-Module/06-module-helpers.js
@@ -2,8 +2,11 @@ function sayHello(user) {
     return alert (`Hello, ${user}`);
   }
 
-  export default sayHello;
-
-  export function greeting(name){
+function greeting(name){
     return alert(`Hello, ${name}`);
   }
+
+  export {
+    sayHello,
+    greeting
+  };
--- 06-Assyncronous/01-asynchronous.js
+++ 06-Assyncronous/01-asynchronous.js
@@ -1,4 +1,5 @@
 // 1. Apa itu synchronous?
+// Syncronous (online lansung) merupakan komunikasi secara langsung seperti halnya Video Call dan Chattting.
 
 
 // 2. Apa itu asynchronous?
@@ -9,8 +10,27 @@
 // halt.
 
 // 3. Dapatkah kita menerapkan konsep asynchronous pada browser?
+// tentu saja bisa, pada dasarnya asynchronous memberikan kemudahan kepada browser untuk menjalankan perintah yang diminta.
+// Kode yang dituliskan secara asynchronous akan dieksekusi di belakang thread utama atau biasa disebut main thread. 
+// Hal tersebut tidak akan membloking proses runtime atau menunggu hingga proses selesai dilakukan. 
+// Sembari menunggu proses tersebut selesai, compiler akan mengeksekusi perintah kode selanjutnya.
+
 // 4. Apa hasil yang akan tercetak pada baris kode dibawah?
+// // first log: 
+// 01-asynchronous.js:24 first log:  2
+// 01-asynchronous.js:24 first log:  3
+// 01-asynchronous.js:24 first log:  4
+// 01-asynchronous.js:24 first log:  5
+// // second log: 
+// 01-asynchronous.js:25 second log:  1
+// 01-asynchronous.js:25 second log:  2
+// 01-asynchronous.js:25 second log:  3
+// 01-asynchronous.js:25 second log:  4
+// 01-asynchronous.js:25 second log:  5
+
 // 5. Apabila terdapat ketidak samaan antara console.log pertama dan kedua, bisakah kalian memberikan penjelasan mengapa hal tersebut bisa terjadi
+// berdasarkan hasil yang ditampilkan kedua console.log memiliki nilai yang sama namun letak perbedaan dari kedua eksekusi tersebut adalah waktu delaynya. dimana untuk second log di set waktu ke 100 second. sedangkan first log tidak.
+
 // 6. Perbaiki baris kode dibawah sehingga ia akan menampilkan angka yang sama
 for (let i = 1; i<=5; i++) {
     console.log("first log: ", i); // 01 - Pertama
